e2b7b18b5e4a444a746235a8472639668aa16fd7
FluxApex <faithnomoread@yahoo.com>
2020-02-26 22:20:40 -0600

    Working on site

--- test/js/main.js
+++ test/js/main.js
@@ -6,15 +6,14 @@ function changeLeftWidth() {
     elem = document.querySelector('.left');
     style = getComputedStyle(elem)[0];
     cl = document.getElementsByClassName('left')
+    alert(cl[0].width + '\n' + cl[1].width + cl['h1'].width);
     if (tog === true) {
         for (var i = 0; i < cl.length; i++) {
             cl[i].style.width = (16 + "px");
-            cl[1].style.transform = ("rotate(90deg)");
         }
     } else {
         for (var i = 0; i < cl.length; i++) {
             cl[i].style.width = (140 + "px");
-            cl[1].style.transform = ("rotate(0deg)");
         }
     }
 }
