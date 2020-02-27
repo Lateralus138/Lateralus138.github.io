4f2756b9e983a5317460ce3741f71267bb77c959
FluxApex <faithnomoread@yahoo.com>
2020-02-26 21:02:43 -0600

    Working on site

--- test/js/main.js
+++ test/js/main.js
@@ -3,8 +3,8 @@ function changeLeftWidth() {
     elem = document.querySelector('.left');
     style = getComputedStyle(elem);
     if (style.width == "140px") {
-        document.getElementsByClassName('left').style.width = "16px";
+        document.getElementsByClassName('left')[0].style.width = "16px";
         return
     }
-    document.getElementsByClassName('left').style.width = "140px";
+    document.getElementsByClassName('left')[0].style.width = "140px";
 }
