16f5731b6103379ef22b0ae0228d771b25fb3265
FluxApex <faithnomoread@yahoo.com>
2020-02-27 17:37:50 -0600

    Working on test site

--- test/js/main.js
+++ test/js/main.js
@@ -1,22 +1,18 @@
-var tog = false;
-
-function changeLeftWidth() {
-    var elem, style, cl;
-    tog = !tog;
-    elem = document.querySelector('.left');
-    style = getComputedStyle(elem)[0];
-    cl = document.getElementsByClassName('left');
-    tag = document.getElementsByTagName('h6');
-    if (tog === true) {
-        for (var i = 0; i < cl.length; i++) {
-            cl[i].style.width = (16 + "px");
-        }
-        for (i = 0; i < tag.length; i++) {
-            tag[i].style.transform = ("rotate(90deg)");
-        }
-    } else {
-        for (var i = 0; i < cl.length; i++) {
-            cl[i].style.width = (140 + "px");
-        }
+function changeLeftMenu() {
+    leftMenu = document.getElementById('left-menu');
+    leftH6 = document.getElementById('left-h4');
+    ibodyWrap = document.getElementById('ibody-wrap');
+    if (getComputedStyle(
+            document.querySelector(
+                '#left-menu')).width.replace(
+            /[^0-9]/g, "") > 16) {
+        ibodyWrap.setAttribute("style", "display: flex;");
+        leftH6.setAttribute("style", "transform: rotate(90deg);display: block;margin: 0;");
+        leftMenu.setAttribute("style", "width: 16px;");
+        return;
     }
+    ibodyWrap.setAttribute("style", "display: none;")
+    leftMenu.setAttribute("style", "width: " + (window.innerWidth - 17) + "px;");
+    leftH6.setAttribute("style", "transform: rotate(0deg);display: inline-block; margin: 0 auto;")
+    leftMenu.setAttribute("style", "width: 100%;text-align: center;");
 }
