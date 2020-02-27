abce406046988f79115b2e6c1fb7da143d435472
FluxApex <faithnomoread@yahoo.com>
2020-02-26 22:13:26 -0600

    Working on site

--- test/js/main.js
+++ test/js/main.js
@@ -9,10 +9,12 @@ function changeLeftWidth() {
     if (tog === true) {
         for (var i = 0; i < cl.length; i++) {
             cl[i].style.width = (16 + "px");
+            cl[i].style.transform = ("rotate(90deg)");
         }
     } else {
         for (var i = 0; i < cl.length; i++) {
             cl[i].style.width = (140 + "px");
+            cl[i].style.transform = ("rotate(0deg)");
         }
     }
 }
