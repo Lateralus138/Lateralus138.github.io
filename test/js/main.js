ddbed8fb4ac792c02c95e6aa093ce3d76f8053bd
FluxApex <faithnomoread@yahoo.com>
2020-02-27 21:33:30 -0600

    Working on test site

--- test/js/main.js
+++ test/js/main.js
@@ -1,3 +1,35 @@
+var addEvent = function(object, type, callback) {
+    if (object == null || typeof(object) == 'undefined') return;
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent("on" + type, callback);
+    } else {
+        object["on" + type] = callback;
+    }
+};
+
+function onResize() {
+    arrow_img = document.getElementById('arrow');
+    leftH6 = document.getElementById('left-h4');
+    slctLeftMenu = document.querySelector('#left-menu');
+    cmptLeftMenu = getComputedStyle(slctLeftMenu);
+    slctArrowImg = document.querySelector('#arrow');
+    cmptArrowImg = getComputedStyle(slctArrowImg);
+    lMenuHeight = onlyNums(cmptLeftMenu.height);
+    lMenuWidth = onlyNums(cmptLeftMenu.width);
+    aImgHeight = onlyNums(cmptArrowImg.height);
+    mTop = ((lMenuHeight / 2) - (aImgHeight / 2))
+    arrow_img.setAttribute("style", "margin-top: " + mTop + "px;width: 12px;float:right;");
+    if (lMenuWidth > 16) {
+        leftH6.setAttribute("style", "margin: 0;transform: rotate(0deg);");
+    }
+}
+
+function onlyNums(string) {
+    return string.replace(/[^0-9]/g, "");
+}
+
 function changeLeftMenu() {
     leftMenu = document.getElementById('left-menu');
     leftH6 = document.getElementById('left-h4');
